ecb9af877aee907641d95496c5677731ef5b2aa9
Farhad-Tanveee <farhadtanveer05u@example.com>
2023-03-27 21:25:12 +0200

    show data from cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,11 +4,21 @@ import "./Cart.css";
 const Cart = ({ cart }) => {
   //   const cart = props.cart; or
   //   const { cart } = props;  option 2
+
   let totalPrice = 0;
   let totalShipping = 0;
+  let quantity = 0;
   for (const product of cart) {
-    totalPrice = totalPrice + product.price;
+    // simple solution to show the product quantity correctly
+    //   if (product.quantity === 0) {
+    //     product.quantity = 1;
+    //   }
+    // another way to solve this
+    product.quantity = product.quantity || 1;
+
+    totalPrice = totalPrice + product.price * product.quantity;
     totalShipping = totalShipping + product.shipping;
+    quantity = quantity + product.quantity;
   }
   const tax = (totalPrice * 7) / 100;
   const grandTotal = totalPrice + totalShipping + tax;
@@ -16,7 +26,7 @@ const Cart = ({ cart }) => {
     <div className="cart">
       <h3 className="text-center my-6 text-3xl">order summery</h3>
       <div className="p-4 text-xl space-y-5">
-        <p>Selected Items: ${cart.length}</p>
+        <p>Selected Items: ${quantity}</p>
         <p>Total Price: ${totalPrice}</p>
         <p>Total Shipping Charge: ${totalShipping}</p>
         <p>Tax: ${tax.toFixed(2)}</p>
