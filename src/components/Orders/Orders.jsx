17ceb4adb64e8ec504e987d93205421db7514450
Farhad-Tanveee <farhadtanveer05u@example.com>
2023-04-06 10:43:49 +0200

    added review item design and data

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import Cart from "../Cart/Cart";
+import Reviewitem from "../Reviewitem/Reviewitem";
+import "./orders.css";
 
 const Orders = () => {
   const cart = useLoaderData();
   return (
     <div>
       <div className="shop-container">
-        <div className="product-container grid grid-cols-3 lg:grid-cols-3 p-8 gap-3">
-          <h2>Orders page:{cart.length} </h2>
+        <div className="review-container">
+          {cart.map((product) => (
+            <Reviewitem key={product.id} product={product}></Reviewitem>
+          ))}
         </div>
         <div className="cart-container bg-secondary">
           <Cart cart={cart}></Cart>
---
+++ src/components/Reviewitem/Reviewitem.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+
+import "./Reviewitem.css";
+
+const Reviewitem = ({ product }) => {
+  const { id, img, price, name, quantity } = product;
+  return (
+    <div className="review-item">
+      <img src={img} alt="" />
+      <div className="review-details flex flex-col flex-grow justify-center">
+        <p>{name}</p>
+        <p>
+          Price: <span className=" text-orange-500 ">${price}</span>
+        </p>
+        <p>
+          Order Quantity: <span className=" text-orange-500 ">{quantity}</span>
+        </p>
+      </div>
+      <div className="flex justify-center items-center mr-5 group">
+        <button className="btn btn-error border-none h-14 w-14 rounded-full bg-[rgba(235,87,87,0.3)]">
+          <FontAwesomeIcon
+            className="text-2xl text-red-700 group-hover:text-white"
+            icon={faTrashAlt}
+          />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Reviewitem;
